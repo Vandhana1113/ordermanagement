1b5818f36e022f9c98a700150a62450e01e1113b
V Vandhana <131697564+Vandhana1113@users.noreply.github.com>
2025-09-08 10:43:52 +0530

    Delete src directory

--- src/Store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import orderReducer from '../Slice/OrderSlice';
-
-export const store = configureStore({
-  reducer: {
-    orders: orderReducer,
-  },
-});
